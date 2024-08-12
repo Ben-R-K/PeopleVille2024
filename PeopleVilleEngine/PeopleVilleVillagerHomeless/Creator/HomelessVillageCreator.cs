704e046fb0b84fd1d8c888aaa20b6d007300ecb6
Mads Højte Rasmussen <madshr2005@gmail.com>
2024-08-12 13:16:59 +0200

    No changes made

---
+++ PeopleVilleEngine/PeopleVilleVillagerHomeless/Creator/HomelessVillageCreator.cs
@@ -0,0 +1,28 @@
+﻿using PeopleVilleEngine.Locations;
+using PeopleVilleEngine;
+using PeopleVilleEngine.Villagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleVilleEngine.Villagers.Creators;
+
+namespace PeopleVilleVillagerHomeless.Creator;
+public class HomelessVillageCreator : IVillagerCreator
+{
+    public bool CreateVillager(Village village)
+    {
+        if(village.Villagers.Count(v => v.HasHome() == false) * 10 > village.Villagers.Count )
+            return false; //No more the 10% can be homeless
+        
+        var random = RNG.GetInstance();
+        if (random.Next(1, 6) != 3)
+            return false; //1 of 6 chance to create a homeless
+        
+        var adult = new AdultVillager(village, random.Next(20, 65));
+        //Add to village
+        village.Villagers.Add(adult);
+        return true;
+    }
+}
