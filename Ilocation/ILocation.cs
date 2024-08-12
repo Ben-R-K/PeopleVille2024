1944874cf92e44830a799acae813305449927a92
HFGF <HFGF@DESKTOP-3V2KPST>
2024-08-12 14:46:12 +0200

    Build ILocation

---
+++ Ilocation/ILocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilocation
+{
+    interface ILocation
+    {
+        public string Name { get; }
+
+        /// <summary>
+        /// The current amount of people in this location.
+        /// </summary>
+        public int CurrentPopulation { get; }
+        /// <summary>
+        /// The max amount of people that can be in this location.
+        /// </summary>
+        public int MaxPopulation { get; }
+    }
+}
