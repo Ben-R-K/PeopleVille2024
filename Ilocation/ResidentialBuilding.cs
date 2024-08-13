49791932df226c040f07f600149368985f7926a4
HFGF <HFGF@DESKTOP-3V2KPST>
2024-08-13 08:32:16 +0200

    Created ILocation

---
+++ Ilocation/ResidentialBuilding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilocation
+{
+    public class ResidentialBuilding : ILocation
+    {
+        public string Name { get; set; }
+
+        public int CurrentPopulation { get; set; }
+
+        public int MaxPopulation { get; set; }
+
+        protected ResidentialBuilding(string name, int maxPopulation)
+        {
+            Name = name;
+            CurrentPopulation = 0;
+            MaxPopulation = maxPopulation;
+        }
+    }
+}
